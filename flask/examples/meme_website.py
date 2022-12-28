da461baedfa78e2696fcc53fba08914316bb401c
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2022-12-28 10:23:40 +0530

    implements a flask site that pulls memes

---
+++ flask/examples/meme_website.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import requests
+import json
+
+app = Flask(__name__)
+
+
+# @app.route("/")
+# def index():
+#     return "<h1>Hello</h1>"
+
+
+def get_meme():
+    url = "https://meme-api.com/gimme"
+    response = json.loads(requests.request("GET", url).text)
+    meme_large = response["url"]
+    subreddit = response["subreddit"]
+    return meme_large, subreddit
+
+
+@app.route("/")
+def index():
+    meme_pic, subreddit = get_meme()
+    return render_template("meme_index.html",
+            meme_pic=meme_pic, subreddit=subreddit)
